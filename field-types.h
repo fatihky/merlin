255db431286e6c74e6a4c34ecfa2d1481a683f83
Fatih Kaya <1994274@gmail.com>
2017-06-08 14:18:17 +0300

    add support for unsigned 64 bit integers

--- field-types.h
+++ field-types.h
@@ -9,6 +9,7 @@ const int FIELD_TYPE_TIMESTAMP = 1;
 const int FIELD_TYPE_INT = 2;
 const int FIELD_TYPE_STRING = 3;
 const int FIELD_TYPE_BOOLEAN = 4;
+const int FIELD_TYPE_BIGINT = 5; // uint64
 
 const int FIELD_ENCODING_NONE = 1;
 const int FIELD_ENCODING_DICT = 2;
--- generic-value.h
+++ generic-value.h
@@ -15,14 +15,17 @@ class GenericValueContainer {
   public:
   int type;
   int64_t i64Val;
+  uint64_t u64Val;
   int iVal;
   string strVal;
   bool bVal;
   GenericValueContainer(int64_t i64Val_): i64Val(i64Val_) { type = FIELD_TYPE_TIMESTAMP; }
+  GenericValueContainer(uint64_t u64Val_): u64Val(u64Val_) { type = FIELD_TYPE_BIGINT; }
   GenericValueContainer(int iVal_): iVal(iVal_) { type = FIELD_TYPE_INT; }
   GenericValueContainer(string strVal_): strVal(strVal_) { type = FIELD_TYPE_STRING; }
   GenericValueContainer(bool bVal_): bVal(bVal_) { type = FIELD_TYPE_BOOLEAN; }
   const inline int64_t getInt64Val () const { return i64Val; }
+  const inline uint64_t getUInt64Val () const { return u64Val; }
   const inline int getIVal() const { return iVal; }
   const inline string getStrVal() const { return strVal; }
   const inline bool getBVal() const { return bVal; }
