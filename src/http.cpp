e7ac48411740ac547ac527d84ed0e40eafa51023
Fatih Kaya <1994274@gmail.com>
2017-06-16 17:25:47 +0300

    implement create table command

--- src/http.cpp
+++ src/http.cpp
@@ -3,6 +3,7 @@
 #include <uWS/uWS.h>
 #include "picojson.h"
 #include "table.h"
+#include "field.h"
 
 using namespace std;
 
@@ -22,13 +23,25 @@ static bool validateRequestBody(uWS::HttpResponse *res, uWS::HttpRequest &req, p
 // command handlers
 static bool commandPing(uWS::HttpResponse *httpRes, uWS::HttpRequest &httpReq, picojson::object &req, picojson::object &res);
 static bool commandShowTables(uWS::HttpResponse *httpRes, uWS::HttpRequest &httpReq, picojson::object &req, picojson::object &res);
+static bool commandCreateTable(uWS::HttpResponse *httpRes, uWS::HttpRequest &httpReq, picojson::object &req, picojson::object &res);
 
 // global server context
-static MerlinHttpServer httpServer = {};
+static MerlinHttpServer httpServer = {
+};
+
+map<string, int> fieldTypes = {
+  {"timestamp", FIELD_TYPE_TIMESTAMP},
+  {"string", FIELD_TYPE_STRING},
+  {"int", FIELD_TYPE_INT}
+};
+map<string, int> encodingTypes = {
+  {"dict", FIELD_ENCODING_DICT}
+};
 
 void httpServerInit() {
   httpServer.commandHandlers["ping"] = commandPing;
   httpServer.commandHandlers["show_tables"] = commandShowTables;
+  httpServer.commandHandlers["create_table"] = commandCreateTable;
 }
 
 void httpHandler(uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes) {
@@ -85,6 +98,12 @@ static bool validateRequestBody(uWS::HttpResponse *res, uWS::HttpRequest &req, p
   return true;
 }
 
+static bool setError(picojson::object &res, string message) {
+  res["stat"] = picojson::value("error");
+  res["error_message"] = picojson::value(message);
+  return true;
+}
+
 static bool commandPing(uWS::HttpResponse *httpRes, uWS::HttpRequest &httpReq, picojson::object &req, picojson::object &res) {
   res["pong"] = picojson::value(true);
   return true;
@@ -102,6 +121,93 @@ static bool commandShowTables(uWS::HttpResponse *httpRes, uWS::HttpRequest &http
   return true;
 }
 
+static bool commandCreateTable(uWS::HttpResponse *httpRes, uWS::HttpRequest &httpReq, picojson::object &req, picojson::object &res) {
+  picojson::array tableNames;
+  string tableName;
+  const auto fields = req["fields"];
+  string err;
+  const auto table = new Table();
+
+  if (!req["name"].is<string>()) {
+    err = "table name is required";
+    goto error;
+  }
+
+  tableName = req["name"].to_str();
+
+  if (httpServer.tables.count(tableName) == 1) {
+    err = "table already exist";
+    goto error;
+  }
+
+  if (!fields.is<picojson::array>()) {
+    err = "fields prop is required.";
+    goto error;
+  }
+
+  for (auto &&field : fields.get<picojson::array>()) {
+    if (!field.is<picojson::object>()) {
+      err = "field must be an object";
+      goto error;
+    }
+
+    auto obj = field.get<picojson::object>();
+    string name;
+    string type;
+    string encoding;
+
+    if (!obj["name"].is<string>()) {
+      err = "name prop required in field definitions";
+      goto error;
+    }
+
+    name = obj["name"].to_str();
+
+    if (!obj["type"].is<string>()) {
+      err = "type prop required in field definitions";
+      goto error;
+    }
+
+    type = obj["type"].to_str();
+
+    if (fieldTypes.count(type) == 0) {
+      err = "invalid field type: " + type;
+      goto error;
+    }
+
+    if (obj["encoding"].is<string>()) {
+      encoding = obj["encoding"].to_str();
+
+      if (encodingTypes.count(encoding) == 0) {
+        err = "invalid encoding type: " + encoding;
+        goto error;
+      }
+
+      if (encoding == "dict" && type != "string") {
+        err = "dict encoding only usable with string fields at the moment";
+        goto error;
+      }
+    }
+
+    const auto mField = new Field(name, fieldTypes[type]);
+
+    if (!encoding.empty()) {
+      mField->encoding = encodingTypes[encoding];
+    }
+
+    table->setField(mField);
+  }
+
+  httpServer.tables[tableName] = table;
+
+  res["created"] = picojson::value(true);
+
+  return true;
+
+  error:
+    return setError(res, err);
+}
+
 int main() {
   uWS::Hub h;
 
@@ -109,6 +215,8 @@ int main() {
   httpServerInit();
 
   h.onHttpRequest(httpHandler);
+
+  cout << "starting http server on :3000" << endl;
 
   h.listen(3000);
   h.run();
